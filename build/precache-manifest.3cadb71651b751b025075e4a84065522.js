2be603a4ea43fe1edb1034379135c51f778739f6
Unknown <bassplayermanrulez@gmail.com>
2019-03-10 18:17:05 +0200

    first build 10/03/19

---
+++ build/precache-manifest.3cadb71651b751b025075e4a84065522.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "af1bf82b9908201d0869c564bd63b95a",
+    "url": "/static/media/avatar-image.af1bf82b.png"
+  },
+  {
+    "revision": "1766e6ba976cc665c8d156d428ffd254",
+    "url": "/static/media/MainMap.1766e6ba.png"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "01372f879a54217818dc",
+    "url": "/static/js/main.d284c930.chunk.js"
+  },
+  {
+    "revision": "df609c4b47e1cd1fb2d6",
+    "url": "/static/js/2.801d3f0b.chunk.js"
+  },
+  {
+    "revision": "01372f879a54217818dc",
+    "url": "/static/css/main.026398c8.chunk.css"
+  },
+  {
+    "revision": "5eabd6303d8bc062b4eacb7dc8491178",
+    "url": "/index.html"
+  }
+];
